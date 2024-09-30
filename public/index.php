1445b257a588258e1953a924bee57e9b0ade44d7
Christian Kirschberg <ckirschberg@gmail.com>
2024-09-30 14:05:58 +0200

    .

--- public/index.php
+++ public/index.php
@@ -8,6 +8,18 @@
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); 
     
     $password = getenv("PASSWORD");
+    $servername = "localhost:3306";
+    $username = "root";
+    
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=nasty", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Connected successfully";
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+
 
     echo $password; 
 ?> 
